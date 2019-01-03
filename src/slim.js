43dac06104f0fd86d6708c8e938209188241457c
Timothy Downs <timothy.downs@luxgroup.com>
2019-01-03 16:27:38 +1100

    ugly trimming

--- src/slim.js
+++ src/slim.js
@@ -1,11 +1,37 @@
 let fetch = require('node-fetch');
 const MAX_PAGES = 5;
 
+function reduceObject(object, includes, deepIncludes, trim, parentChain) {
+  let reduced = {};
+  let obj = Object.entries(object);
+  obj.forEach(function(entry) {
+    let each_key = entry[0];
+    let each_value = entry[1];
+    if (Array.isArray(each_value) && trim.indexOf(each_key) != -1) {
+      reduced[each_key] = [each_value[0]];
+    } else if (deepIncludes[each_key]) {
+      reduced[each_key] = reduceObject(each_value, deepIncludes[each_key], {}, trim, parentChain.concat(each_key));
+    } else {
+      if (includes.indexOf(each_key) != -1) {
+        reduced[each_key] = each_value;
+      }
+    }
+  });
+  return reduced;
+}
+
 async function slim(req, res, next) {
   let url = 'https://' + process.env.API_DOMAIN + req.originalUrl.replace('/slim', '');
   console.log("url is", url);
-  console.log("url is", req.query);
   let includes = req.query.includes ? req.query.includes.split(',') : [];
+  let trim = req.query.trim ? req.query.trim.split(',') : [];
+  let nestedIncludes = {};
+  for (let i = 0; i < includes.length; i++) {
+    let splitIncludes = includes[i].split(".");
+    if (splitIncludes.length > 1) {
+      nestedIncludes[splitIncludes[0]] = splitIncludes.slice(1);
+    }
+  }
   let joined = [];
   try {
     for (let i = 1; i < MAX_PAGES; i++) {
@@ -17,18 +43,8 @@ async function slim(req, res, next) {
       let resp_json = await resp.json();
       let result = [];
       for (let i = 0; i < resp_json.result.length; i++) {
-        result.push({});
-        let obj = Object.entries(resp_json.result[i]);
-        obj.forEach(function(entry) {
-          let each_key = entry[0];
-          let each_value = entry[1];
-          if (includes.indexOf(each_key) != -1) {
-            result[i][each_key] = each_value;
-          }
-        });
+        result.push(reduceObject(resp_json.result[i], includes, nestedIncludes, trim, []));
       }
-      Object.entries(resp_json.result).forEach(function(entry) {
-      });
       joined = joined.concat(result);
       if (!resp_json.result) {
         return res.status(resp_json.status).json(resp_json);
